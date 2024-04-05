a57dccf02e84cdb40ae27d21ba27a5da7e0b79bf
Sophia Willows <20146550+sophiabits@users.noreply.github.com>
2024-04-05 13:40:21 +1300

    Validate auth scheme as well

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,16 @@
-const INVALID_AUTH_HEADER_MESSAGE =
+export const INVALID_AUTH_HEADER_MESSAGE =
   'You have provided an invalid Authorization header. The Rye SDK expects a complete header value such as "Basic XXX". API calls will silently fail and return `undefined` until a valid header is provided. In future versions of the SDK, this will throw an error.';
 
 export function warnIfAuthHeaderInvalid(authHeader: string): void {
   if (!authHeader.includes(' ') || authHeader.lastIndexOf(' ') !== authHeader.indexOf(' ')) {
     warn(INVALID_AUTH_HEADER_MESSAGE);
+    return;
+  }
+
+  const [authType] = authHeader.split(' ');
+  if (!['basic', 'bearer'].includes(authType.toLowerCase())) {
+    warn(INVALID_AUTH_HEADER_MESSAGE);
+    return;
   }
 }
 
