f85dbee0e7c1589a6d2f24983d0bcd8bded6f3dd
Sophia Willows <20146550+sophiabits@users.noreply.github.com>
2024-08-12 10:13:15 +1200

    Select `canonicalDomain` from `requestStoreByURL`

--- src/gql/requestStoreByURL.ts
+++ src/gql/requestStoreByURL.ts
@@ -3,6 +3,7 @@ import { graphql } from '../graphql';
 export const REQUEST_STORE_BY_URL_MUTATION = graphql(`
   mutation RequestStoreByURL($input: RequestStoreByURLInput!) {
     requestStoreByURL(input: $input) {
+      canonicalDomain
       requestID
     }
   }
