58fb7920a3a5b9cf5079928ce9ceda762a885116
Sophia Willows <20146550+sophiabits@users.noreply.github.com>
2025-04-10 12:48:59 +1200

    Do not re-export `gql.ts`

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 export * from './constants';
-export * from './graphql/gql';
 export * from './graphql/graphql';
-export * from './graphql/index';
 export * from './ryeClient';
 export * from './types';
